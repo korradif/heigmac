21ae1ffc1115704c81eccd6633319a8430da176f
Simon Flückiger <simon.fluckiger@hotmail.com>
2019-12-28 18:19:39 +0100

    implement mongodbdao

--- MovieSolution/MoviesGlobalResources/MongoDBDAO.cs
+++ MovieSolution/MoviesGlobalResources/MongoDBDAO.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 
 namespace MoviesGlobalResources
 {
     public class MongoDBDAO
     {
+        private MongoClient _client;
+        private IMongoCollection<BsonDocument> _movieCollection;
+        private const string _connectionString = "mongodb://SAUCOMPUTER01:27017/admin";
+        private const string _collectionName = "MovieCollection";
+        private const string _dbName = "Movie";
+
+        public MongoDBDAO()
+        {
+            _client = new MongoClient(_connectionString);
+            _movieCollection = _client.GetDatabase(_dbName).GetCollection<BsonDocument>(_collectionName);
+        }
+
+        public BsonDocument GetFilm(int movieID)
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("id", movieID);
+            return _movieCollection.Find(filter).FirstOrDefault();
+        }
+
+
     }
 }
