450b914afc831075e9b5436ff78215d4b21744f2
frederic <frederiic@outlook.com>
2019-12-28 17:20:10 +0100

    set webhook

---
+++ MovieSolution/Telegram.Bot.Examples.DotNetCoreWebHook/BotConfiguration.cs
@@ -0,0 +1,11 @@
+namespace Telegram.Bot.Examples.DotNetCoreWebHook
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; set; }
+        //1021492488:AAHzn9Sw4g8Ntyh8p7hr6GWA40nb0639sVU
+        public string Socks5Host { get; set; }
+
+        public int Socks5Port { get; set; }
+    }
+}
